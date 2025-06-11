99ddb218286acda278fee2a78c89ee48265407e8
Shubhamsahar <kumarshubham765484@gmail.com>
2025-06-11 15:51:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	
+    let charCount = new Map();
+
+    
+    for (let char of s) {
+        charCount.set(char, (charCount.get(char) || 0) + 1);
+    }
+
+    
+    for (let char of s) {
+        if (charCount.get(char) === 1) {
+            return char;
+        }
+    }
+    return null;
+}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
